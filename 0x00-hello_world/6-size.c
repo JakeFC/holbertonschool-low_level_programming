4b983cc3f060e0fe724d2f3fd518de297799047c
JakeFC <jacobchavera@yahoo.com>
2021-02-04 04:15:20 +0000

    printing sizes of input in C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	long long int ll;
 	float f;
 
-	printf("Size of a char: %lli byte(s).\n", (long long int)sizeof(c));
-	printf("Size of an int: %lli byte(s).\n", (long long int)sizeof(i));
-	printf("Size of a long int: %lli byte(s).\n", (long long int)sizeof(li));
-	printf("Size of a long long int: %lli byte(s).\n", (long long int)sizeof(ll));
-	printf("Size of a float: %lli byte(s).\n", (long long int)sizeof(f));
+	printf("Size of a char: %lli byte(s)\n", (long long int)sizeof(c));
+	printf("Size of an int: %lli byte(s)\n", (long long int)sizeof(i));
+	printf("Size of a long int: %lli byte(s)\n", (long long int)sizeof(li));
+	printf("Size of a long long int: %lli byte(s)\n", (long long int)sizeof(ll));
+	printf("Size of a float: %lli byte(s)\n", (long long int)sizeof(f));
 
 	return (0);
 }
