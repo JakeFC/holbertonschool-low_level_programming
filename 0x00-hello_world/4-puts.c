2b882b043f8dc4df8f51af5f25fbd2c97769cb48
JakeFC <jacobchavera@yahoo.com>
2021-02-04 02:14:56 +0000

    uncompiled puts C file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,8 @@
  */
 int main(void)
 {
-	char str1[80]
+	char str1[50] = ("\"Programming is like building a multilingual puzzle");
 
-	strcpy(str1, '"Programming is like building a multilingual puzzle');
 	puts(str1);
 		return (0);
 }
