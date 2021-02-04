40fc81a4ac15ff032c96b0082434d0e963603c84
JakeFC <jacobchavera@yahoo.com>
2021-02-04 01:41:00 +0000

    uncompiled puts C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints text to cmd line
+ *Return: 0
+ */
+int main(void)
+{
+	int puts("Programming is like building a multilingual puzzle");
+		return (0);
+}
