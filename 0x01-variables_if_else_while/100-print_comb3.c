9655c8315a54752981f0acea00e275267387b474
JakeFC <jacobchavera@yahoo.com>
2021-02-05 20:27:14 +0000

    for if if putchar combos of 4 characters

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,16 +8,15 @@ int main(void)
 {
 	int n;
 
-	for (n = 1; ((n / 10) + '0') < ((n % 10) + '0'); n++)
+	for (n=1; n <= 89; n++)
 	{
-		while (n <= 89)
+		if ((n / 10) < (n % 10))
 		{
 			{
 			putchar((n / 10) + '0');
 			putchar((n % 10) + '0');
-			n++;
 			}
-			if (n < 90)
+			if (n < 89)
 			{
 			putchar(',');
 			putchar(' ');
