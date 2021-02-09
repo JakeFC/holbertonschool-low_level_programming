368f420c39eaad992a5e2ed341b154c4021213ba
JakeFC <jacobchavera@yahoo.com>
2021-02-09 19:37:41 +0000

    print the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ void times_table(void)
 		b = 0;
 		while (b <= 9)
 		{
-			_putchar(((a * b) % 10) + '0');
+			_putchar((((a * b) % 10) + '0'));
 			if (b < 9)
 			{
 				_putchar(',');
@@ -25,7 +25,7 @@ void times_table(void)
 					_putchar(' ');
 					if (((a * b) / 10 + 0) >= 1)
 					{
-						_putchar(a * (b + 1) / 10 + '0');
+						_putchar((a * (b + 1) / 10 + '0'));
 						d++;
 					}
 					d++;
