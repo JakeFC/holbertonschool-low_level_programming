029bb9872dd774f718e687a265e0102ce6b20b5e
JakeFC <jacobchavera@yahoo.com>
2021-02-09 18:54:56 +0000

    while looping every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,19 +9,19 @@ void jack_bauer(void)
 	int b;
 
 	a = 0;
-		while (a <= 23)
+	while (a <= 23)
+	{
+		b = 0;
+		while (b <= 59)
 		{
-				b = 0;
-			while (b <= 59)
-			{
-				_putchar(a / 10 + '0');
-				_putchar(a % 10 + '0');
-				_putchar(':');
-				_putchar(b / 10 + '0');
-				_putchar(b % 10 + '0');
-				_putchar('\n');
-				b++;
-			}
-			a++;
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			_putchar(':');
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+			_putchar('\n');
+			b++;
 		}
+		a++;
+	}
 }
