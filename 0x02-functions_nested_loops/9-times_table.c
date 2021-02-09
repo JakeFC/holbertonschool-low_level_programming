dfb3f47dcaabe5555508fa160b57ef4b580a35d6
JakeFC <jacobchavera@yahoo.com>
2021-02-09 20:37:24 +0000

    moving variable definitions

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,3 +1,4 @@
+#include "holberton.h"
 
 /**
  * times_table - prints the 9 times table
@@ -14,9 +15,9 @@ void times_table(void)
 	while (a <= 9)
 	{
 		b = 0;
+		e = (a * b) % 10;
 		while (b <= 9)
 		{
-			e = (a * b) % 10;
 			_putchar(e + '0');
 			if (b < 9)
 			{
@@ -25,9 +26,9 @@ void times_table(void)
 				while (d < 2)
 				{
 					_putchar(' ');
+					f = (a * (b + 1) / 10);
 					if (((a * b) / 10 + 0) >= 1)
 					{
-						f = (a * (b + 1) / 10);
 						_putchar(f + '0');
 						d++;
 					}
