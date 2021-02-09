1b61b024caae72d8dccf1a2dad32d66aa96d95f2
JakeFC <jacobchavera@yahoo.com>
2021-02-09 23:56:21 +0000

    added standard library

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,20 +1,23 @@
+#include <stdio.h>
 #include "holberton.h"
 
 /**
  * print_to_98 - prints all numbers n to 98
+ * @n: input
  * Return: 0
  */
 void print_to_98(int n)
-[
-
-	while (n < 98 || n > 98)
-			if (n > 99 || n < -99)
-			{
-				putchar(n / 100);
-				if (n > 9 || n < -9)
-				{
-					putchar((n % 100) / 10);
-				}
-				n++;
-			}
+{
+	while (n < 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+	while (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+	printf("98\n");
+	return (0);
 }
