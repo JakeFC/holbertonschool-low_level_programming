7f24a740d01c254d1ffd69831ede474ceca261d6
JakeFC <jacobchavera@yahoo.com>
2021-02-09 17:27:03 +0000

    printing and returning last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,16 @@
-#include "holberton.h"
-#include <stdlib.h>
 /**
  * print_last_digit - prints the last digit of a #
- * c is input
- * a is last digit of c
- * b is abs of a
+ * @c: is input
  * Return: absolute value of a
  */
 int print_last_digit(int c)
 {
-	int a = abs(c % 10);
-	int b = abs(a);
+	int a;
 
+	if (c < 0)
+		a = (c * -1) % 10;
+	else
+		a = c % 10;
 	_putchar(a + '0');
-	return (b);
+	return (a);
 }
