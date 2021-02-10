8c917f24939cab53fd60477a4dcc16253f3f87b9
JakeFC <jacobchavera@yahoo.com>
2021-02-10 02:49:33 +0000

    fixed += relationship

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,8 +2,9 @@
 
 /**
  * main - adds all multiples of 3 and 5 below 1024
+ * Return: 0
  */
-void main(void)
+int main(void)
 {
 	int a;
 	int b;
@@ -12,8 +13,9 @@ void main(void)
 	while (a < 1024)
 	{
 		if (a % 3 == 0 || a % 5 == 0)
-			a += b;
+			b += a;
 		a++;
 	}
-	printf('%d/n', b)
+	printf("%d\n", b);
+	return (0);
 }
