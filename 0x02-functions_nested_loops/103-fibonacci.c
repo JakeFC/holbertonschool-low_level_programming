738b9d5a4d6f0b0944ab9d53ec8c29f9f76a8338
JakeFC <jacobchavera@yahoo.com>
2021-02-10 04:38:37 +0000

    first try

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of all even fibonaccis under 4 mil
+ * Return: 0
+ */
+
+int main(void)
+{
+	long a = 0;
+	long b = 1;
+	long c;
+	long d;
+	long long e;
+
+	for (d = 1; d <= 50; d++)
+	{
+		c = a + b;
+		if (d < 50 && c < 4000000 && c % 2 == 0)
+			e += c;
+		a = b;
+		b = c;
+	}
+	printf("%lld\n", e);
+	return (0);
+}
