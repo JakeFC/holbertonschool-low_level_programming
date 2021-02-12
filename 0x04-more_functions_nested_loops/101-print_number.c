81d49772ef0127ed7e4110f33943631b145f8ee9
JakeFC <jacobchavera@yahoo.com>
2021-02-12 01:44:29 +0000

    moved sign change to the top of function

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,16 +6,17 @@
  */
 void print_number(int n)
 {
-	int a = ((n / 1000) % 10);
-	int b = ((n / 100) % 10);
-	int c = ((n / 10) % 10);
-	int d = (n % 10);
-
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
+
+	int a = ((n / 1000) % 10);
+	int b = ((n / 100) % 10);
+	int c = ((n / 10) % 10);
+	int d = (n % 10);
+
 	if (n > 999)
 		_putchar(a + '0');
 	if (n > 99)
