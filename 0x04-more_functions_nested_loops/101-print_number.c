89313c94a08a7d8b3e1e4e82ae91cefbbe77db92
JakeFC <jacobchavera@yahoo.com>
2021-02-12 02:01:51 +0000

    translated each int into ascii char

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,22 +14,22 @@ void print_number(int n)
 		n = -n;
 	}
 
-	a = ((n / 1000) % 10);
-	b = ((n / 100) % 10);
-	c = ((n / 10) % 10);
-	d = (n % 10);
-	e = ((n / 10000) % 10);
-	f = ((n / 100000) % 10);
+	a = ((n / 1000) % 10) + 48;
+	b = ((n / 100) % 10) + 48;
+	c = ((n / 10) % 10) + 48;
+	d = (n % 10) + 48;
+	e = ((n / 10000) % 10) + 48;
+	f = ((n / 100000) % 10) + 48;
 
 	if (n > 99999)
-		_putchar(f + '0');
+		_putchar(f);
 	if (n > 9999)
-		_putchar(e + '0');
+		_putchar(e);
 	if (n > 999)
-		_putchar(a + '0');
+		_putchar(a);
 	if (n > 99)
-		_putchar(b + '0');
+		_putchar(b);
 	if (n > 9)
-		_putchar(c + '0');
-	_putchar(d + '0');
+		_putchar(c);
+	_putchar(d);
 }
