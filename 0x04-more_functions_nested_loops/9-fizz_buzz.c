a3f526a6885e74381b2db17d6ad5fd5d9a689f67
JakeFC <jacobchavera@yahoo.com>
2021-02-12 00:30:46 +0000

    fixed else statements

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,15 @@ int main(void)
 	for (a = 1; a <= 100; a++)
 	{
 		if ((a % 5 == 0) && (a % 3 == 0))
-		  printf("FizzBuzz");
-		else if (a % 5 == 0)
-			printf("Buzz");
-		else if (a % 3 == 0)
-			printf("Fizz");
-		else
-			printf("%d", a);
+		{
+			printf("FizzBuzz");
+			else if (a % 5 == 0)
+				printf("Buzz");
+			else if (a % 3 == 0)
+				printf("Fizz");
+			else
+				printf("%d", a);
+		}
 		if (a < 100)
 			putchar(' ');
 	}
