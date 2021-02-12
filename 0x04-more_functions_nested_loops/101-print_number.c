0bb109d6f289f0ea41d9cdec089e9ee878f6257e
JakeFC <jacobchavera@yahoo.com>
2021-02-12 01:41:04 +0000

    first try

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_number - prints an integer
+ * @n: input to print
+ */
+void print_number(int n)
+{
+	int a = ((n / 1000) % 10);
+	int b = ((n / 100) % 10);
+	int c = ((n / 10) % 10);
+	int d = (n % 10);
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n > 999)
+		_putchar(a + '0');
+	if (n > 99)
+		_putchar(b + '0');
+	if (n > 9)
+		_putchar(c + '0');
+	_putchar(d + '0');
+}
