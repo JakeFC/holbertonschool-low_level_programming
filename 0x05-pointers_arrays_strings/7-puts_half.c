5d378af902cc47e7b078d37b3b74baf7481185f3
JakeFC <jacobchavera@yahoo.com>
2021-02-16 23:57:02 +0000

    printing second half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints second half of a string
+ * @str: input string
+ */
+void puts_half(char *str)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (str[a] != '\0')
+		a++;
+	a--;
+	for (b = 0; str[b] != '\0'; b++)
+	{
+		if (b >= a / 2)
+			_putchar(str[b]);
+	}
+	_putchar('\n');
+}
