ef3425ae764b1f13370633256bff138e9a8652a0
JakeFC <jacobchavera@yahoo.com>
2021-02-18 19:32:56 +0000

    reversing an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverses an array of ints
+ * @a: string input
+ * @n: number of array elements
+ */
+void reverse_array(int *a, int n)
+{
+	int c, t;
+
+	a[n - 1] = '\0';
+	n--;
+	for (c = 0; c <= n / 2; c++, n--)
+	{
+		t = a[c];
+		a[c] = a[n];
+		a[n] = t;
+	}
+}
