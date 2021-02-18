f8305dce23a99b44bcb4ed74772db0ea80797191
JakeFC <jacobchavera@yahoo.com>
2021-02-18 20:12:13 +0000

    converting lower char to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - changes lowercases to uppercases in a string
+ * @char *: string input
+ * Return: pointer to char
+ */
+char *string_toupper(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+		if (s[a] > 96 && s[a] < 123)
+			s[a] -= 32;
+	return (s);
+}
