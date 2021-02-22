3d17c50c9195b5b535bec8354357b4628a6f75a3
JakeFC <jacobchavera@yahoo.com>
2021-02-22 23:40:04 +0000

    added sum of second diagonal and formatting

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,15 +2,18 @@
 #include "holberton.h"
 
 /**
- * print_diagsums - prints the sums of the diagonals of square matrix
+ * print_diagsums - prints the sums of the diagonals of a square matrix
  * @a: square input array
  * @size: length and height of a
  */
 void print_diagsums(int *a, int size)
 {
-	unsigned int b, sum;
+	int b;
+	unsigned sum1, sum2;
 
-	for (b = 0, sum = 0; b < size; b++)
-		sum += a[b][b];
-	printf("%d", sum);
+	for (b = 0, sum1 = 0; b < size * size; b += size + 1)
+		sum1 += a[b];
+	for (b = size - 1, sum2 = 0; b < size * size - size + 1; b += size - 1)
+		sum2 += a[b];
+	printf("%d, %d\n", sum1, sum2);
 }
