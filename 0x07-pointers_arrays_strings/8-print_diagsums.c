23dc6a552d222137b0197e5d197c3489f9accb2b
JakeFC <jacobchavera@yahoo.com>
2021-02-22 23:18:26 +0000

    adding the diagonal of a square matrix

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_diagsums - prints the sums of the diagonals of square matrix
+ * @a: square input array
+ * @size: length and height of a
+ */
+void print_diagsums(int *a, int size)
+{
+	unsigned int b, sum;
+
+	for (b = 0, sum = 0; b < size; b++)
+		sum += a[b][b];
+	return (sum);
+}
