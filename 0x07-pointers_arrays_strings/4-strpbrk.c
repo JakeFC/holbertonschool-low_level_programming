169d0ea3a26367ba04db88ec40d076b9802af76a
JakeFC <jacobchavera@yahoo.com>
2021-02-22 22:28:36 +0000

    removed references to unused variable

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,11 +9,10 @@
 char *_strpbrk(char *s, char *accept)
 {
 	unsigned int a, b;
-	int c;
 
 	for (a = 0; s[a] != '\0'; a++)
 	{
-		for (b = 0, c = 0; accept[b] != '\0'; b++)
+		for (b = 0; accept[b] != '\0'; b++)
 			if (s[a] == accept[b])
 				return (s + a);
 	}
