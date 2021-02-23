c789913085896246815ecc9aba6f10900a656c4a
JakeFC <jacobchavera@yahoo.com>
2021-02-23 02:47:42 +0000

    changed b to double for more digits, changed b increment to happen when finding a number, and reset variable values when a separate number is found earlier in loop

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,18 +7,28 @@
  */
 int _atoi(char *s)
 {
-	int a, b, c;
+	int a, c, i;
+	double b;
 
 	for (a = 0; s[a] != '\0';)
 		a++;
 	a--;
-	c = 0;
-	for (b = 1; a >= 0; a--, b *= 10)
+	for (b = 1, i = 0, c = 0; a >= 0; a--)
 	{
 		if (s[a] <= '9' && s[a] >= '0')
-			c += ((s[a] - 48) * b);
+		{
+			if (c == 1 && (s[a + 1] < 48 || s[a + 1] > 57))
+			{
+				c = 0;
+				i = 0;
+				b = 1;
+			}
+			i += ((s[a] - 48) * b);
+			b *= 10;
+			c = 1;
+		}
 		if (s[a] == '-')
-			c *= -1;
+			i = -i;
 	}
-	return (c);
+	return (i);
 }
