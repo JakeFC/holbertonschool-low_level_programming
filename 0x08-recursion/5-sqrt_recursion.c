81b830bfbe0dbb173892e8b512a954a7f1e4fdde
JakeFC <jacobchavera@yahoo.com>
2021-02-24 21:13:08 +0000

    added comment for second function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,13 +3,19 @@
 /**
  * _sqrt_recursion - returns the square root of a number
  * @n: the number
- * Return: square root of n
+ * Return: square root of n or -1 if no natural root
  */
 int _sqrt_recursion(int n)
 {
 	return (_square(0, n));
 }
 
+/**
+ * _square - returns the square root of a number
+ * @x: root to test for square value
+ * @n: value to compare to x times x
+ * Return: square root of n or -1 if no natural root
+ */
 int _square(int x, int n)
 {
 	int k = x * x;
