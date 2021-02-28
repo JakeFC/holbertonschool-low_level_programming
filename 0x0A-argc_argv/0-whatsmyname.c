83dc50422b96baa5d3bf618f19e6b8db17407bfa
JakeFC <jacobchavera@yahoo.com>
2021-02-28 23:56:54 +0000

    printing first value of argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints its own name
+ * @argc: number of arguments
+ * @argv: string of cmd line arguments
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
