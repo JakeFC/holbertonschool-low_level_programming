466fa21b29d262c61832d7b9a84f8f3e67309e62
JakeFC <jacobchavera@yahoo.com>
2021-03-02 23:51:20 +0000

    dynamically allocating memory for an array of given size and type

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars and initializes with a
+ * specific char
+ * @size: number of bytes in the array
+ * @c: char which will fill the array
+ * Return: pointer to array, or NULL if size is 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *a = malloc(size);
+
+	if (size == 0)
+		return (00);
+	for (i = 0; i < size; i++)
+		a[i] = c;
+	return (a);
+}
