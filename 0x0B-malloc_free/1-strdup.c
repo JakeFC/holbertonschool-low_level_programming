debb4b255bdbdbe5740fc93cb080801f4962ea93
JakeFC <jacobchavera@yahoo.com>
2021-03-03 00:10:51 +0000

    allocating memory for a new string after finding string length to create a copy

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - creates and returns a pointer to a copy of the inputted string
+ * @str: string input
+ * Return: pointer to copy of str, or NULL if str is NULL or insufficient memory
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int i, l;
+
+	if (str == NULL)
+		return (0);
+	for (l = 0; str[l] != '\0';)
+		l++;
+	s = malloc(l + 1);
+	if (s == NULL)
+		return (0);
+	for (i = 0; i <= l; i++)
+		s[i] = str[i];
+	return (s);
+}
