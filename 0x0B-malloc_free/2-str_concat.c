0631dc23c06b134607a01bfeaa0c4b19d7112c43
JakeFC <jacobchavera@yahoo.com>
2021-03-03 00:23:02 +0000

    dynamically allocating space for a concatenation of two inputted strings after finding strlen

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - creates a copy of s1 with s2 added to the end
+ * @s1: string input
+ * @s2: second string input
+ * Return: pointer to new string, or 0 if insufficient memory
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *c;
+	int a, b, i, ii;
+
+	for (a = 0; s1[a] != 00;)
+		a++;
+	for (b = 0; s2[b] != 00;)
+		b++;
+	c = malloc(a + b + 1);
+	if (c == NULL)
+		return (0);
+	for (i = 0; i < a; i++)
+		c[i] = s1[i];
+	for (ii = 0; i < (a + b + 1); i++, ii++)
+		c[i] = s2[ii];
+	return (c);
+}
