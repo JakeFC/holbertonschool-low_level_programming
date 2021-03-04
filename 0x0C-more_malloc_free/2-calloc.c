8e56f29d3395a9625a7d076da615dd56fd6924d4
JakeFC <jacobchavera@yahoo.com>
2021-03-04 23:46:52 +0000

    changed endpoint when initializing to index of last byte instead of last element

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	s = malloc(nmemb * size);
 	if (s == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		s[i] = 00;
 	return (s);
 }
