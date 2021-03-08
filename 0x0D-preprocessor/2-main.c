4f9ff764cc8e69c986ffff00bdffd1ce1d0487e7
JakeFC <jacobchavera@yahoo.com>
2021-03-08 23:45:46 +0000

    added in helper function manually

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -11,3 +11,20 @@ int main(void)
 	_puts(__FILE__);
 	return (0);
 }
+
+/**
+ * _puts - prints a string
+ * @str: string input
+ */
+void _puts(char *str)
+{
+	int a;
+
+	a = 0;
+	while (*(str + a) != '\0')
+	{
+		_putchar(*(str + a));
+		a++;
+	}
+	_putchar('\n');
+}
