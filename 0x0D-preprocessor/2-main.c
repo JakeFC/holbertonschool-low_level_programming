34a3dc379b1fa2dc3e2400903bc29c7a98b82e6f
JakeFC <jacobchavera@yahoo.com>
2021-03-08 23:44:09 +0000

    using a predefined macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * main - prints the name of this file
+ * Return: always 0
+ */
+int main(void)
+{
+	_puts(__FILE__);
+	return (0);
+}
