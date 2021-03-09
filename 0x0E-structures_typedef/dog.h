7a88e64c6d7fed94c55d1b274334ba0c7b3a8930
JakeFC <jacobchavera@yahoo.com>
2021-03-09 02:28:59 +0000

    added prototypes for printing

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,6 +18,8 @@ typedef struct dog
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+int _putchar(char c);
+void _puts(char *str);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
