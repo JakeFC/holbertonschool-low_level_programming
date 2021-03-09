17f19e150a6e40f96a6015c9e91fb73285a2aabf
JakeFC <jacobchavera@yahoo.com>
2021-03-09 01:47:02 +0000

    added task prototypes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,3 +15,12 @@ struct dog
 };
 
 #endif
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
