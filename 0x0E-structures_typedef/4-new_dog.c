42d73356481100ce7980baa71910c6883e421ff2
JakeFC <jacobchavera@yahoo.com>
2021-03-09 02:50:15 +0000

    creating a new struct from inputs with a function

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: dog name
+ * @age: age of dog
+ * @owner: name of owner
+ * Return: pointer to new dog, or NULL on failure
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	return (dog);
+}
