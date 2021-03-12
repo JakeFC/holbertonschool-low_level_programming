e33e6b72036622ba6e008cf5e02730701ff90790
JakeFC <jacobchavera@yahoo.com>
2021-03-12 17:13:12 +0000

    added struct for #3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,17 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+/**
+ * struct op - contains matched letters and functions
+ * @letter: one-char character string assigning type
+ * @f: function to run for that type
+ */
+typedef struct op
+{
+	char *letter;
+	void (*f)();
+} op_t;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
