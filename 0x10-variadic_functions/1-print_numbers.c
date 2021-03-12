0d93f53e6eb857d8526a9125f65c780c9e1761c1
JakeFC <jacobchavera@yahoo.com>
2021-03-12 00:52:35 +0000

    printing every argument of a given quantity, with a constant separator

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers followed by a newline
+ * @separator: string printed between characters
+ * @n: number of ints passed
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int i;
+
+	if (n == 0)
+		return;
+	va_start(nums, n);
+	for (i = 0; i < n - 1; i++)
+		if (separator)
+			printf("%d%s", va_arg(nums, int), separator);
+		else
+			printf("%d", va_arg(nums, int));
+	printf("%d\n", va_arg(nums, int));
+}
