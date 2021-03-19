45a77b9d615e7bd066b03cb180fd114bee0d9d3b
JakeFC <jacobchavera@yahoo.com>
2021-03-19 02:43:03 +0000

    using weird language to call a function before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * print_hax - prints a string before main function
+ */
+void __attribute__((constructor))print_hax()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
