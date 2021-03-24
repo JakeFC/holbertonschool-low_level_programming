10f2ede7e04f2f39444858e960ec40dddf0468e7
JakeFC <jacobchavera@yahoo.com>
2021-03-24 15:13:08 +0000

    finding where a loop starts in a list

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the loop in a linked list
+ * @head: first node in the list
+ * Return: address where loop starts, or NULL if none
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	char counter, check = 0;
+	listint_t *trav, *tort, *hare, *prev;
+
+	if (!head)
+		return (NULL);
+	if (!head->next)
+		return (NULL);
+	trav = head;
+	for (; trav; trav = trav->next)
+	{
+		counter = 0, tort = head, hare = head->next;
+		while (tort && hare && hare->next)
+		{
+			if (tort == trav)
+				if (counter == 0)
+					counter = 1;
+				else
+				{
+					check = 1;
+					break;
+				}
+			if (tort == hare)
+				break;
+			if (!hare->next || !hare->next->next)
+				return (NULL);
+			prev = tort;
+			tort = tort->next, hare = hare->next->next;
+		}
+		if (check == 1)
+		{
+			return (prev);
+		}
+	}
+	return (NULL);
+}
