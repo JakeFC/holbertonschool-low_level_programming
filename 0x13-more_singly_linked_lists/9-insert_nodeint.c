46e0f8d4bc4bad44a32546afd986012a08a829a0
JakeFC <jacobchavera@yahoo.com>
2021-03-24 00:09:36 +0000

    initialized node in first if statement before setting head equal to it

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,9 +19,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new = malloc(sizeof(listint_t));
 		if (!new)
 			return (NULL);
-		*head = new;
 		new->n = n;
 		new->next = NULL;
+		*head = new;
 		return (new);
 	}
 	tmp = *head;
