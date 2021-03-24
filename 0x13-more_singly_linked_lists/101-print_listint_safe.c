54b4c97f019365c15d851274eea6678a0563b7cf
JakeFC <jacobchavera@yahoo.com>
2021-03-24 21:19:26 +0000

    rewrote after doing last task

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,24 +1,62 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints all elements of a listint_t list without looping
- * @head: listint_t list
- * Return: number of nodes
+ * loop_finder - finds the loop in a linked list
+ * @head: first node in the list
+ * Return: address where loop starts, or NULL if none
+ */
+listint_t *loop_finder(listint_t *head)
+{
+	listint_t *tort = head, *hare = head;
+
+	if (!head)
+		return (NULL);
+	while (hare && hare->next)
+	{
+		tort = tort->next, hare = hare->next->next;
+		if (!hare)
+			return (NULL);
+		if (tort == hare)
+			for (hare = head; 1;)
+			{
+				if (tort == hare)
+					return (tort);
+				tort = tort->next;
+				hare = hare->next;
+			}
+	}
+	return (NULL);
+}
+/**
+ * print_listint_safe - prints a singly linked list that can contain a loop
+ * @head: pointer to first node of list
+ * Return: number of nodes in list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *tmp;
-	int x;
+	char counter = 0;
+	listint_t *loop, *trav;
+	int n = 0;
 
 	if (!head)
 		exit(98);
-	tmp = head->next;
-	for (x = 0; tmp; x++)
+	trav = (listint_t *)head;
+	loop = loop_finder(trav);
+	while (trav)
 	{
-		printf("%d\n", tmp->n);
-		tmp = tmp->next;
-		if (tmp == head)
-			return (x + 1);
+		if (loop)
+		{
+			if (trav == loop && counter == 1)
+			{
+				printf("-> [%p] %d\n", (void *)trav, trav->n);
+				break;
+			}
+			if (trav == loop && counter == 0)
+				counter = 1;
+		}
+		printf("[%p] %d\n", (void *)trav, trav->n);
+		trav = trav->next;
+		n++;
 	}
-	return (x);
+	return (n);
 }
