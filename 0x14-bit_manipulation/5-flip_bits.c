94a8b065de20d84da2320d22c2e7dd6c7cda18c1
JakeFC <jacobchavera@yahoo.com>
2021-03-26 00:34:58 +0000

    counting unique bits of two number

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - returns the number of different bits between two numbers
+ * @n: decimal number
+ * @m: decimal number
+ * Return: number
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, sum;
+	unsigned long int uniq = n^m;
+
+	for (i = 0, sum = 0; i < 64; uniq >>= 1, i++)
+	{
+		if (uniq & 1)
+			sum++;
+	}
+	return (sum);
+}
