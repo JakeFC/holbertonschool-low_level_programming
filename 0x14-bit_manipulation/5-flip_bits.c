569485654c982207460e8b46971c53b539c7fcb1
JakeFC <jacobchavera@yahoo.com>
2021-03-26 00:35:45 +0000

    fixed betty spacing

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,7 +9,7 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i, sum;
-	unsigned long int uniq = n^m;
+	unsigned long int uniq = n ^ m;
 
 	for (i = 0, sum = 0; i < 64; uniq >>= 1, i++)
 	{
