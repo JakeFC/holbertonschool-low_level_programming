340749778dd471467ec671537a5e5a04c063c878
JakeFC <jacobchavera@yahoo.com>
2021-03-31 03:24:54 +0000

    fixed betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 }
 /**
  * _strlen - finds the length of a string
- * @str: string input
+ * @s: string input
  * Return: string length, or 0 on error
  */
 int _strlen(char *s)
