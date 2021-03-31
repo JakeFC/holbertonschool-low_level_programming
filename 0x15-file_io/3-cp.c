6891e96f711985f9e30e6046e0a2d358b969347d
JakeFC <jacobchavera@yahoo.com>
2021-03-31 07:35:14 +0000

    checked for read error and edited to fit 40 lines

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,10 +12,7 @@ int main(int argc, char **argv)
 	char buf[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
 	{
@@ -29,20 +26,24 @@ int main(int argc, char **argv)
 		close(from), exit(99);
 	}
 	for (count = read(from, buf, 1024); count > 0; count = read(from, buf, 1024))
+	{
+		if (count < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
 		if (write(to, buf, count) < 0)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(from), close(to), exit(99);
 		}
+	}
 	if (close(from) < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
 		close(to), exit(100);
 	}
 	if (close(to) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to), exit(100);
 	return (0);
 }
