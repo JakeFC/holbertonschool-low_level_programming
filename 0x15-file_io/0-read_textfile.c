0a0051aff13f32eac9eacf5dc929e55d5dbcecee
JakeFC <jacobchavera@yahoo.com>
2021-03-31 02:40:28 +0000

    added intermediary buffer and tried using count instead of letters for write amount

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,15 +10,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t count;
+	char *buf;
 
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	count = read(fd, STDOUT_FILENO, letters);
+	buf = malloc(letters);
+	count = read(fd, buf, letters);
+	if (write(STDOUT_FILENO, buf, count) < 0)
+		count = 0;
 	close(fd);
-	if (count < 0)
-		return (0);
+	free(buf);
 	return (count);
 }
