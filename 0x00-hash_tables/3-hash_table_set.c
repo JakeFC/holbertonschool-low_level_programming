98c59f1f3b4db94c5ac945eb04e15405194a9e73
JakeFC <jacobchavera@yahoo.com>
2021-07-28 23:58:09 +0000

    added function to check for duplicate keys and replaced their value, rather than creating a duplicate node

--- 0x00-hash_tables/3-hash_table_set.c
+++ 0x00-hash_tables/3-hash_table_set.c
@@ -9,7 +9,7 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node;
+	hash_node_t *node, *same_key;
 	unsigned long int index;
 
 	if (!ht || !key[0])
@@ -27,10 +27,42 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	strcpy(node->value, value);
 	node->next = NULL;
 	index = key_index((unsigned char *)key, ht->size);
+	same_key = find_key(ht, index, node->key);
+	if (same_key)
+	{
+		free(same_key->value);
+		same_key->value = node->value;
+		free(node->key);
+		free(node);
+		return (1);
+	}
 	if (ht->array[index] != NULL)
 	{
 		node->next = ht->array[index];
 	}
 	ht->array[index] = node;
 	return (1);
+}
+
+/**
+ * find_key - finds the hash_node_t node with matching key if it exists
+ * @ht: pointer to the hash table
+ * @index: index to search
+ * @key: key string to match
+ * Return: pointer to the node, or NULL
+ */
+hash_node_t *find_key(hash_table_t *ht, unsigned long int index, char *key)
+{
+	hash_node_t *node;
+
+	node = ht->array[index];
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			return (node);
+		}
+		node = node->next;
+	}
+	return (NULL);
 }
